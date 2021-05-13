9c60ff777d3f0b04c5a9ec3f58da7db22fa2476f
Bridger Herman <herma582@umn.edu>
2021-05-13 09:47:38 -0500

    Add background color chooser

---
+++ Assets/BackgroundColor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundColor : MonoBehaviour
+{
+    public CUIColorPicker picker;
+    public Camera mainCam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        picker.Color = mainCam.backgroundColor;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mainCam.backgroundColor = picker.Color;
+    }
+}
