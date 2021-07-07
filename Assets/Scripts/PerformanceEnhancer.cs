4a55a1cbfb64cccb9649c71515e97f484818a2ba
Bridger Herman <herma582@umn.edu>
2021-07-07 17:00:37 -0500

    Improve wording on performance enhancer

--- Assets/Scripts/PerformanceEnhancer.cs
+++ Assets/Scripts/PerformanceEnhancer.cs
@@ -5,16 +5,24 @@ using UnityEngine.Rendering;
 using IVLab.ABREngine;
 using Newtonsoft.Json.Linq;
 
+/// <summary>
+/// Performance enhancement for older computers running ABR. Essentially, one
+/// frame is rendered every 10 seconds *unless* the ABR state is changed OR
+/// there is mouse/keyboard input.
+/// </summary>
 public class PerformanceEnhancer : MonoBehaviour
 {
     // Render every 10 seconds (600 frames)
-    private int slowInterval = 600;
+    [SerializeField] private int slowFrameInterval = 600;
 
     // Quick interval for rendering (every 1 frame)
-    private int quickInterval = 1;
+    [SerializeField] private int fastFrameInterval = 1;
+
+    // Target frame rate for the app
+    [SerializeField] private int targetFrameRate = 60;
 
     // Frames since speedy render called
-    private int framesSinceSpeedyRender = 0;
+    private int framesSinceSpeedup = 0;
 
     // Frames to wait before slowing down again
     private int framesToBeFast = 10;
@@ -25,36 +33,36 @@ public class PerformanceEnhancer : MonoBehaviour
         // Assign callback for when ABREngine receives new state
         ABREngine.Instance.OnStateChanged += StateUpdated;
         QualitySettings.vSyncCount = 0;
-        Application.targetFrameRate = 60;
-        OnDemandRendering.renderFrameInterval = slowInterval;
+        Application.targetFrameRate = targetFrameRate;
+        OnDemandRendering.renderFrameInterval = slowFrameInterval;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (framesSinceSpeedyRender > framesToBeFast)
+        if (framesSinceSpeedup > framesToBeFast)
         {
-            OnDemandRendering.renderFrameInterval = slowInterval;
+            OnDemandRendering.renderFrameInterval = slowFrameInterval;
         }
         else
         {
-            framesSinceSpeedyRender += 1;
+            framesSinceSpeedup += 1;
         }
 
         if (Input.anyKey)
         {
-            SpeedyRender();
+            SpeedUp();
         }
     }
 
     void StateUpdated(JObject _state)
     {
-        SpeedyRender();
+        SpeedUp();
     }
 
-    void SpeedyRender()
+    void SpeedUp()
     {
-        OnDemandRendering.renderFrameInterval = quickInterval;
-        framesSinceSpeedyRender = 0;
+        OnDemandRendering.renderFrameInterval = fastFrameInterval;
+        framesSinceSpeedup = 0;
     }
 }
