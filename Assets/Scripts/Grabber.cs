9a97a1f5fec297a1254c563572cc23f54061c6d2
Bridger Herman <herma582@umn.edu>
2021-09-16 14:17:00 -0500

    Add two-handed scaling for VR

--- Assets/Scripts/Grabber.cs
+++ Assets/Scripts/Grabber.cs
@@ -6,8 +6,23 @@ using UnityEngine.XR;
 [RequireComponent(typeof(Collider))]
 public class Grabber : MonoBehaviour
 {
-    private Transform oldParent = null;
+    public Grabber otherHand = null;
+
     private GameObject collidingObject = null;
+    private GameObject grabbedObject = null;
+    
+    [HideInInspector] public bool grabbing = false;
+    [HideInInspector] public bool scaling = false;
+
+    private Vector3 lastHandDifference = Vector3.zero;
+    private Quaternion lastAngle = Quaternion.identity;
+    private float lastDist = 0.0f;
+    private Matrix4x4 lastMidMat = Matrix4x4.identity;
+    private Matrix4x4 grabOffset = Matrix4x4.identity;
+    private Matrix4x4 lastMatrix = Matrix4x4.identity;
+
+    private float initialDist = 0.0f;
+    private Vector3 initialScale = Vector3.one;
 
     [SerializeField] private bool rightHand = true;
     [SerializeField] private Material selectedMaterial;
@@ -33,6 +48,8 @@ public class Grabber : MonoBehaviour
             | InputDeviceCharacteristics.TrackedDevice;
         InputDevices.GetDevicesWithCharacteristics(desiredCharacteristics, thisHandControllers);
 
+        bool pickUp = false;
+        bool drop = false;
         foreach (var device in thisHandControllers)
         {
             bool triggerValue;
@@ -40,14 +57,68 @@ public class Grabber : MonoBehaviour
             {
                 if (device.TryGetFeatureValue(CommonUsages.gripButton, out triggerValue) && triggerValue)
                 {
-                    collidingObject.transform.parent = transform;
+                    pickUp = true;
                 }
                 else
                 {
-                    collidingObject.transform.parent = oldParent;
+                    drop = true;
                 }
             }
         }
+
+        if (pickUp && !(grabbing || scaling))
+        {
+            PickupObject();
+        }
+        if (drop)
+        {
+            DropObject();
+        }
+
+        Matrix4x4 deltaMatrix;
+
+        Vector3 currentHandDifference = transform.position - otherHand.transform.position;
+        float currentDist = Vector3.Magnitude(currentHandDifference);
+
+        float distDelta = currentDist - lastDist;
+        Quaternion rotDelta = Quaternion.FromToRotation(lastHandDifference, currentHandDifference);
+
+        lastDist = currentDist;
+        lastHandDifference = currentHandDifference;
+
+        if (grabbing && grabbedObject != null && !otherHand.scaling)
+        {
+            Matrix4x4 currentMatrix = this.transform.localToWorldMatrix;
+            deltaMatrix =  currentMatrix * lastMatrix.inverse;
+            lastMatrix = currentMatrix;
+
+            Matrix4x4 newObjMat = deltaMatrix * grabbedObject.transform.localToWorldMatrix;
+            grabbedObject.transform.FromMatrix(newObjMat);   
+        }
+
+        if (scaling && otherHand.grabbedObject != null)
+        {
+            float pDist = currentDist / initialDist;
+
+            Vector3 averagePos = (this.transform.position + otherHand.transform.position)/2;
+            Vector3 between = otherHand.transform.position - this.transform.position;
+            Vector3 averageUp = (this.transform.up + otherHand.transform.up) / 2;
+
+            Vector3 averageForward = Vector3.Cross(between, averageUp.normalized);
+            averageUp = -Vector3.Cross(between, averageForward.normalized);
+
+            Quaternion averageRot = Quaternion.LookRotation(averageForward.normalized,averageUp);
+            float sharedScale = initialScale.x * pDist;
+
+            Matrix4x4 currentMidMat = Matrix4x4.TRS(averagePos, averageRot, sharedScale*Vector3.one);
+            deltaMatrix = currentMidMat * lastMidMat.inverse;
+
+            lastMidMat = currentMidMat;
+            Matrix4x4 newObjMat = deltaMatrix * otherHand.grabbedObject.transform.localToWorldMatrix;
+            otherHand.grabbedObject.transform.FromMatrix(newObjMat);
+        }
+
+        lastMatrix = this.transform.localToWorldMatrix;
     }
 
     void OnTriggerEnter(Collider other)
@@ -55,18 +126,58 @@ public class Grabber : MonoBehaviour
         if (other.GetComponent<Grabbable>() != null)
         {
             collidingObject = other.gameObject;
-            oldParent = collidingObject.transform.parent;
             this.GetComponent<MeshRenderer>().material = selectedMaterial;
         }
     }
     void OnTriggerExit(Collider other)
     {
-        if (collidingObject != null)
-        {
-            collidingObject.transform.parent = oldParent;
-        }
-        oldParent = null;
+        DropObject();
         collidingObject = null;
         this.GetComponent<MeshRenderer>().material = nonSelectedMaterial;
+    }
+
+    void PickupObject()
+    {
+        if (collidingObject != null)
+        {
+            if (!otherHand.grabbing)
+            {
+                grabbing = true;
+                grabbedObject = collidingObject;
+            }
+            else
+            {
+                scaling = true;
+                lastDist = Vector3.Distance(transform.position, otherHand.transform.position);
+                initialDist = lastDist;
+                initialScale = collidingObject.transform.localScale;
+                lastAngle = collidingObject.transform.rotation;
+
+                float pDist = initialDist / initialDist;
+
+                Vector3 averagePos = (this.transform.position + otherHand.transform.position) / 2;
+                Vector3 between = otherHand.transform.position - this.transform.position;
+                Vector3 averageUp = (this.transform.up + otherHand.transform.up) / 2;
+
+                Vector3 averageForward = Vector3.Cross(between, averageUp);
+                averageUp = -Vector3.Cross(between, averageForward);
+
+                Quaternion averageRot = Quaternion.LookRotation(averageForward, averageUp);
+                float sharedScale = initialScale.x * pDist;
+
+                lastMidMat = Matrix4x4.TRS(averagePos, averageRot, sharedScale * Vector3.one);
+            }
+        }
+    }
+
+    void DropObject()
+    {
+        grabbedObject = null;
+        grabbing = false;
+        scaling = false;
+        if (otherHand.scaling)
+        {
+            otherHand.PickupObject();
+        }
     }
 }
